34b958654bbc7ff822d72c5e4218ecaf77066c08
munergenis <munergenis@gmail.com>
2025-04-09 16:00:41 +0200

    feat: CalendarApp calendar functionality

--- src/components/CalendarApp.tsx
+++ src/components/CalendarApp.tsx
@@ -1,60 +1,78 @@
+import { useState } from 'react';
+import './CalendarApp.css';
+
 const CalendarApp = () => {
+  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const monthOfYear = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const currentDate = new Date();
+
+  const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const jsDay = new Date(currentYear, currentMonth, 1).getDay();
+  const firstDayOfMonth = (jsDay + 6) % 7;
+
+  const handlePrevMonth = () => {
+    setCurrentMonth((pMonth) => (pMonth === 0 ? 11 : pMonth - 1));
+    setCurrentYear((pYear) => (currentMonth === 0 ? pYear - 1 : pYear));
+  };
+  const handleNextMonth = () => {
+    setCurrentMonth((pMonth) => (pMonth === 11 ? 0 : pMonth + 1));
+    setCurrentYear((pYear) => (currentMonth === 11 ? pYear + 1 : pYear));
+  };
+
   return (
     <div className="calendar-app">
       <div className="calendar">
         <h1 className="heading">Calendar</h1>
 
         <div className="navigate-date">
-          <h2 className="month">May,</h2>
-          <h2 className="year">2025</h2>
+          <h2 className="month">{monthOfYear[currentMonth]},</h2>
+          <h2 className="year">{currentYear}</h2>
           <div className="buttons">
-            <button>&lt;</button>
-            <button>&gt;</button>
+            {/* TODO change for real icons */}
+            <button onClick={handlePrevMonth}>&lt;</button>
+            <button onClick={handleNextMonth}>&gt;</button>
           </div>
         </div>
 
         <div className="weekdays">
-          <span>Mon</span>
-          <span>Tue</span>
-          <span>Wed</span>
-          <span>Thu</span>
-          <span>Fri</span>
-          <span>Sat</span>
-          <span>Sun</span>
+          {daysOfWeek.map((day, i) => (
+            <span key={i}>{day}</span>
+          ))}
         </div>
 
         <div className="days">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-          <span>7</span>
-          <span>8</span>
-          <span>9</span>
-          <span>10</span>
-          <span>11</span>
-          <span>12</span>
-          <span>13</span>
-          <span>14</span>
-          <span>15</span>
-          <span>16</span>
-          <span>17</span>
-          <span>18</span>
-          <span>19</span>
-          <span>20</span>
-          <span>21</span>
-          <span>22</span>
-          <span>23</span>
-          <span>24</span>
-          <span>25</span>
-          <span>26</span>
-          <span>27</span>
-          <span>28</span>
-          <span>29</span>
-          <span>30</span>
-          <span>31</span>
+          {[...Array(firstDayOfMonth).keys()].map((_, i) => (
+            <span key={`empty-${i}`} />
+          ))}
+          {[...Array(daysInMonth).keys()].map((day) => (
+            <span
+              key={day + 1}
+              className={`${
+                currentDate.getFullYear() === currentYear &&
+                currentDate.getMonth() === currentMonth &&
+                currentDate.getDate() === day + 1 &&
+                'current-day'
+              }`}
+            >
+              {day + 1}
+            </span>
+          ))}
         </div>
       </div>
 
@@ -72,18 +90,16 @@ const CalendarApp = () => {
             </select>
           </div>
 
-          <div className="title input">
-            <div className="event-popup-title">Title</div>
-            <input
-              className="title"
-              name="title"
-              type="text"
-              placeholder="Enter a title"
-              maxLength={60}
-            />
-          </div>
+          <input
+            className="title"
+            name="title"
+            type="text"
+            placeholder="Enter a title"
+            maxLength={60}
+          />
 
           <button className="event-popup-btn">Add event</button>
+          {/* TODO change for real icons */}
           <button className="close-event-popup">X</button>
         </div>
 
@@ -94,6 +110,7 @@ const CalendarApp = () => {
           </div>
           <div className="event-title">Descent campaign</div>
           <div className="event-buttons">
+            {/* TODO change for real icons */}
             <button>✏️</button>
             <button>X</button>
           </div>
