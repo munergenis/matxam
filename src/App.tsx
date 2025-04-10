e2f4edbbef02459c047c68623d6ac70c7ff208d7
munergenis <munergenis@gmail.com>
2025-04-10 17:26:22 +0200

    feat: replace MainApp with direct rendering in App component and remove MainApp file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import MainApp from './components/App/MainApp';
+import Content from './components/App/Content/Content';
+import Login from './components/Auth/Login/Login';
+import { Tab } from './types/calendarTypes';
+import Tabs from './components/App/Tabs/Tabs';
+import { tabs } from './data/tabs';
+import { useAuth } from './hooks/useAuth';
+import { useState } from 'react';
 
 const App = () => {
+  const { currentUser, login, logout } = useAuth();
+  const [activeTab, setActiveTab] = useState<Tab>(tabs[1]);
+
   return (
     <div className="container">
-      <MainApp />
-      {/* <CalendarApp /> */}
+      {currentUser ? (
+        <div>
+          <Tabs
+            tabs={tabs}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <Content
+            currentUser={currentUser}
+            activeTab={activeTab}
+            logout={logout}
+          />
+        </div>
+      ) : (
+        <Login login={login} />
+      )}
     </div>
   );
 };
